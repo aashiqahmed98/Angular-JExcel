dc69c08678511ffa96ee75d63036435e34f59cb6
AashiqinCode <aashiqahmed10101@ggmail.com>
2020-04-21 13:47:30 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,89 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import * as jexcel from "jexcel";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error("Method not implemented.");
+
+  }
   title = 'project-jexcel';
+
+  ngAfterViewInit() {
+    // The json file is used as string when within quotes and as object without them
+    let companies=`[{
+      "name":"Aashiq",
+      "role":"Dev",
+      "age":21,
+      "Indian":true,
+      "languages":["Tamil","English"]
+  
+  },
+  
+  {
+      "name":"Muhsin",
+      "role":"Dev-Manager",
+      "age":50,
+      "Indian":true,
+      "languages":["Tamil","English","Urdu"]
+  }
+  ]`
+  console.log(JSON.parse(companies)[0].name);
+  
+  console.log(
+      `[{
+      "name":"Aashiq",
+      "role":"Dev",
+      "age":21,
+      "Indian":true,
+      "languages":["Tamil","English"]
+  
+  },
+  
+  {
+      "name":"Muhsin",
+      "role":"Dev-Manager",
+      "age":50,
+      "Indian":true,
+      "languages":["Tamil","English","Urdu"]
+  }
+  ]`)
+
+  
+    jexcel(document.getElementById("spreadsheet"), {
+      data: [{
+        "name":"Aashiq",
+        "role":"Dev",
+        "age":21,
+        "Indian":true,
+        "languages":["Tamil","English"]
+    
+    },
+    
+    {
+        "name":"Muhsin",
+        "role":"Dev-Manager",
+        "age":50,
+        "Indian":true,
+        "languages":["Tamil","English","Urdu"]
+    }
+    ],
+      columns: [
+        { type: "dropdown", width: "80px",title:"Name", source: ["Aashiq","Muhsin"] },
+        { type: "text", width: "80px",title:"Role", render: "square" },
+        {type: "text", width: "80px",title:"Age", render: "square"},
+        {type: "checkbox", width: "80px",title:"Indian", render: "square"},
+        {type: "text", width: "80px",title:"Languages", render: "square"}
+      ],
+      license: '39130-64ebc-bd98e-26bc4',
+      minDimensions: [5,5]
+    });
+  }
+  
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {AngularMaterial} from 'src/shared/angular-material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterial
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
