6360d5547f0565a01bef1552a85be115aa564847
AashiqinCode <aashiqahmed10101@ggmail.com>
2020-04-22 12:47:04 +0530

    working grid

--- src/app/staticdata/staticdata.component.ts
+++ src/app/staticdata/staticdata.component.ts
@@ -11,20 +11,13 @@ export class StaticdataComponent implements OnInit {
   constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
-  //   this.http.get("assets/user.json").subscribe(
-  //     data =>{
-  //       console.log(data);
-  //      this.rowData = data;
-  // })
-
- this.rowData="https://api.jsonbin.io/b/5e9fe554435f5604bb45dce5";
+  
+ this.rowData=this.http.get("https://raw.githubusercontent.com/AashiqinCode/Angular-JExcel/Aashiq/src/assets/user.json");
 }
   columnDefs = [
     {field: 'name',header:"Name",filter:true},
     {field: 'value',header:"Value",filter:true }
 ];
 
-rowData:any=[];
-
-
+rowData:any;
 }
